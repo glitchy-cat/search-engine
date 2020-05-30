3176d77bc83010ba23bf98562cf5db1ae2461c60
Jimmy Bull <jimmybull18@gmail.com>
2020-05-30 00:00:12 -0400

    removed some links from head element

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+var resultContainer = document.getElementById("results");
+var btn = document.getElementById("btn");
+
+btn.addEventListener("click", function () {
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+    ourRequest.onload = function () {
+        var ourData = JSON.parse(ourRequest.responseText);
+            renderHTML(ourData);
+    };
+    ourRequest.send();
+});
+
+function renderHTML(data) {
+    
+
+    resultContainer.insertAdjacentHTML('beforeend', 'testing123');
+}
