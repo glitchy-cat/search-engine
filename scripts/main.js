c5f9e08c4baab3337b1e85e9587ad98c0f58020e
Jimmy Bull <jimmybull18@gmail.com>
2020-06-06 12:18:44 -0400

    Narrowed search parameters and got search to return results

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,23 @@
 document.getElementById('getText').addEventListener('click', getText);
 
+
+
 function getText() {
-    fetch('https://api.rawg.io/api/games')
+    var game = document.querySelector('#site-search').value;
+    var url = 'https://api.rawg.io/api/games?page_size=5&search=' + encodeURIComponent(game);
+    url = url.trim();
+    fetch(url)
         .then((res) => res.json())
         .then((data) => {
             console.log(data)
             let output = '<h2>Results</h2>';
-            data.forEach(function(results) {
+            data.results.forEach(function(post) {
+                console.log(post)
                 output += `
                 <div>
-                <h2> ${results.name}</h2>
-                <img src="${results.background_image}"></img>
-                <p>${results.description}</p>
+                <h2> ${post.name}</h2>
+                <img src="${post.background_image}"></img>
+                <p>${post.description}</p>
                 </div>
             `;
             });
