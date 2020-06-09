dcb29d8d52b10f43f8b5299bb80cb047b34f9f18
Jimmy Bull <jimmybull18@gmail.com>
2020-06-08 20:50:39 -0400

    Centered results to middle of page as well as added clickable button to search results

--- scripts/main.js
+++ scripts/main.js
@@ -2,13 +2,13 @@ document.getElementById('getText').addEventListener('click', getText);
 
 function getText() {
     var game = document.querySelector('#site-search').value;
-    var url = 'https://api.rawg.io/api/games?page_size=5&search=' + encodeURIComponent(game);
+    var url = 'https://api.rawg.io/api/games?search=' + encodeURIComponent(game);
     url = url.trim();
     fetch(url)
         .then((res) => res.json())
         .then((data) => {
             console.log(data)
-            let output = '<h2 class="text-center">Results</h2>';
+            let output = `<h2 class=" col-md-12 text-center">Results</h2><br>`;
             data.results.forEach(function (post) {
                 console.log(post)
                 output += `
@@ -16,6 +16,7 @@ function getText() {
                     <div class = "well text-center">
                     <img src="${post.background_image}"></img>
                     <h2> ${post.name}</h2>
+                    <a onclick="gameSelected('${post.id}')" class="btn btn-primary" href="#">Game Details</a>
                     </div>
                 </div>
             `;
