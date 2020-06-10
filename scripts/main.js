ca8034573f064ad762de0c640aedebc48e4bbeac
Jimmy Bull <jimmybull18@gmail.com>
2020-06-10 10:24:05 -0400

    Added markup for game details page

--- scripts/main.js
+++ scripts/main.js
@@ -5,18 +5,18 @@ function getText() {
     var url = 'https://api.rawg.io/api/games?search=' + encodeURIComponent(game);
     url = url.trim();
     fetch(url)
-        .then((res) => res.json())
+        .then((response) => response.json())
         .then((data) => {
             console.log(data)
             let output = `<h2 class=" col-md-12 text-center">Results</h2><br>`;
             data.results.forEach(function (post) {
-                console.log(post)
+                console.log(post.id)
                 output += `
-                <div class="col-md-3">
-                    <div class = "well text-center">
+                <div class="col-md-4">
+                    <div class ="well text-center">
                     <img src="${post.background_image}"></img>
                     <h2> ${post.name}</h2>
-                    <a onclick="gameSelected('${post.id}')" class="btn btn-info" href="#">Game Details</a>
+                    <a onclick="gameSelected('${post.id}')" class="btn btn-info" href="game.html">Game Details</a>
                     </div>
                 </div>
             `;
@@ -25,3 +25,42 @@ function getText() {
         })
         .catch((err) => console.log(error))
 }
+
+function gameSelected(id) {
+    sessionStorage.setItem('gameId', id);
+    window.location = 'game.html';
+    return false;
+}
+
+function getGame() {
+    let gameId = sessionStorage.getItem('gameId');
+
+    var url = 'https://api.rawg.io/api/games/' + encodeURIComponent(gameId);
+    url = url.trim();
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) =>
+            console.log(data));
+            let game = data; 
+    let details = `
+            <div class ="row">
+                <div class ="col-md-4">
+                    <img src ="${game.background_image}" class="thumbnail">
+                </div>
+                <div class ="col-md-8">
+                    <h2>${game.name}</h2>
+                    <ul class="list-group">
+                    <li class="list-group-item"><strong>Released</strong>: ${game.released}</li>
+                    <li class="list-group-item"><strong>Rating</strong>: ${game.rating}</li>
+                    <li class="list-group-item"><strong>Platforms</strong>: ${game.platforms}</li> 
+                    <li class="list-group-item"><strong>Description</strong>: ${game.description}</li>
+
+                    </ul>
+                </div>
+            </div>
+            <a href="index.html" class="btn btn-success">Back to search</a>
+            `;
+            document.getElementById('game').innerHTML = details;
+            
+    
+};
