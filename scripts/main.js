257395ccd4a94f68e8d8de74bd7a286a0ef5dc39
Jimmy Bull <jimmybull18@gmail.com>
2020-06-13 12:23:32 -0400

    Adjusted botstrap classes to better fit spacing between cards and images inside

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-document.getElementById('getText').addEventListener('click', getText);
+document.getElementById('getSearch').addEventListener('click', getText);
 
 function getText() {
     var game = document.querySelector('#site-search').value;
@@ -11,11 +11,11 @@ function getText() {
             let output = `<h2 class=" col-md-12 text-center">Results</h2><br>`;
             data.results.forEach(function (post) {
                 console.log(post.id)
-                output += `
-                <div class="col-md-4">
+                output += `           
+                <div class="card bg-light mb-3 col-md-3">
                     <div class ="well text-center">
                     <img src="${post.background_image}"></img>
-                    <h2> ${post.name}</h2>
+                    <h4> ${post.name}</h2>
                     <a onclick="gameSelected('${post.id}')" class="btn btn-info" href="game.html">Game Details</a>
                     </div>
                 </div>
@@ -43,7 +43,7 @@ function getGame() {
             console.log(data.id)
 
             let game = `
-            <div class ="row">
+            <div class ="row justify-content-center">
                 <div class ="col-md-4">
                     <img src ="${data.background_image}" class="thumbnail">
                 </div>
@@ -51,7 +51,7 @@ function getGame() {
                     <h2>${data.name}</h2>
                     <ul class="list-group">
                     <li class="list-group-item"><strong>Released</strong>: ${data.released}</li>
-                    <li class="list-group-item"><strong>Rating</strong>: ${data.rating}</li>
+                    <li class="list-group-item"><strong>Rating</strong>: ${data.rating} out of 5</li>
                     <li class="list-group-item"><strong>Platforms</strong>: ${data.platforms}</li> 
                     <li class="list-group-item"><strong>Description</strong>: ${data.description}</li>
 
